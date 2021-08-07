1cfd2c6d3ecc38cc6109efd4b98254c836181c41
Sebastian <sebastianalvarado246801@gmail.com>
2021-08-07 10:14:41 -0500

    feature/mutations

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
+// import Middleware
+const { authMiddleware } = require('./utils/auth')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
@@ -11,10 +13,11 @@ const app = express()
 // create a new Apollo server and pass in out schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
-//integrate our Apollo server with the Express application as middleware
+// integrate our Apollo server with the Express application as middleware
 async function startup() {
   await server.start()
   server.applyMiddleware({ app })
